d6ec6e32ae100b9b6d32ecfe6bd1f6782c97d979
Fabian-Martinez1 <fabianmartinezrincon.123@gmail.com>
2022-11-04 18:19:52 -0300

    agregando strems

--- src/PokeFabo.java
+++ src/PokeFabo.java
@@ -1,12 +1,40 @@
 import Pokemones.*;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 // Para la visibilidad dentro del mismo paquete
 
 public class PokeFabo {
+
 	public static void main(String[] args) {
+		
+		List<Integer> numeros =  new ArrayList<Integer>();
+		
+		numeros.add(2);
+		numeros.add(3);
+		numeros.add(4);
+		numeros.add(5);
+		numeros.add(2);
+		
+		//filtrarNumeros(numeros).forEach(System.out::println);
+		mapearNumeros(numeros).forEach(System.out::println);
 		
 		
 	}
-	void empezandoPartida() {
+	static List<Integer> mapearNumeros(List<Integer> numeros){
+		return numeros.stream()
+				.map(alumno->alumno+4)
+				.collect(Collectors.toList());
+	}
+	
+	static List<Integer> filtrarNumeros(List<Integer> numeros) {
+		
+		return numeros.stream()
+				.filter(numero->numero == 2)
+				.collect(Collectors.toList());
+	}
+	
+	static void empezandoPartida() {
 		Entrenador traine = new Entrenador();
 		traine.nombreEntrenador("FaboHop");
 		
@@ -21,4 +49,5 @@ public class PokeFabo {
 		
 		//charmander.atacarAraniazo();
 	}
+	
 }
